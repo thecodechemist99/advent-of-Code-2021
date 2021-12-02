6da654059e4e92afb22cb1f59ba4b2eb95f4002b
Florian Beck <florian.f.beck@web.de>
2021-12-02 14:24:58 +0100

    Removed obsolete code

--- Day 1/Puzzle 1/puzzle1.c
+++ Day 1/Puzzle 1/puzzle1.c
@@ -11,19 +11,10 @@ int main (void)
   FILE *fp; // declare file pointer
   fp = fopen(FILENAME, "r"); // open file
 
-  char file[MAX_LINES][MAX_LINE_LENGTH];
   char line[MAX_LINE_LENGTH];
 
-  int i, j;
+  int i;
   /* Prefill with empty characters */
-  for (i = 0; i < MAX_LINES; i++)
-  {
-    for (j = 0; j < MAX_LINE_LENGTH; j++)
-    {
-      file[i][j] = '\0';
-    }
-  }
-
   for (i = 0; i < MAX_LINE_LENGTH; i++)
   {
     line[i] = '\0';
@@ -59,36 +50,3 @@ int main (void)
 
   return 1;
 }
-
-// int main (void)
-// {
-//   const char *file = parseFile("input.txt");
-
-//   if (file != NULL)
-//   {
-//     int i, lastValue, increased;
-//     increased = 0;
-
-//     /* check for increases */
-//     for (i = 0; i < MAX_LINES; i++)
-//     {
-//       printf("Line %d: %s", i, &file[i]);
-//       if (!i)
-//         lastValue = atoi(&file[i]);
-//       else
-//       {
-//         if (atoi(&file[i]) > lastValue)
-//           increased++;
-//         lastValue = atoi(&file[i]);
-//       }
-//     }
-
-//     printf("The number of increases is %d.", increased);
-//   }
-//   else
-//   {
-//     /* Error Handling */
-//   }
-
-//   // free(&file);
-// }
