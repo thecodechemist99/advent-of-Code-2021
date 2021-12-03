c3accad922c136fc8bdcfa09a5fcc02998265f3d
Florian Beck <florian.f.beck@web.de>
2021-12-03 13:45:10 +0100

    Epsilon rate implemented and tested

--- Day 3/puzzle1.c
+++ Day 3/puzzle1.c
@@ -4,28 +4,30 @@
 #include <math.h>
 
 #define FILENAME "test.txt"
-#define MAX_LINE_LENGTH 7 // up to 12 characters plus \n and \0
+#define MAX_LINE_LENGTH 5
 #define MAX_LINES 13 // up to 1000 + 1
 
 char** readFile(char fileName[])
 {
-  int i, j;
+  int i, j, lines, lineLength;
   FILE *fp; // declare file pointer
   fp = fopen(fileName, "r"); // open file
+  lines = MAX_LINES + 1;
+  lineLength = MAX_LINE_LENGTH + 2;
 
   /* Create array and allocate memory */
-  char ** file = malloc(MAX_LINES * sizeof(char*));
-  for (i = 0; i < MAX_LINES; i++)
-    file[i] = malloc (MAX_LINE_LENGTH * sizeof(char));
+  char ** file = malloc(lines * sizeof(char*));
+  for (i = 0; i < lines; i++)
+    file[i] = malloc (lineLength * sizeof(char));
 
-  char line[MAX_LINE_LENGTH];
+  char line[lineLength];
 
   /* Prefill with empty characters */
-  for (i = 0; i < MAX_LINES; i++)
-  for (j = 0; j < MAX_LINE_LENGTH; j++)
+  for (i = 0; i < lines; i++)
+  for (j = 0; j < lineLength; j++)
     file[i][j] = '\0';
 
-  for (i = 0; i < MAX_LINE_LENGTH; i++)
+  for (i = 0; i < lineLength; i++)
     line[i] = '\0';
 
   /* check for empty file */
@@ -36,7 +38,7 @@ char** readFile(char fileName[])
     {
       /* read line and check for increases */
       strcpy(file[i], line);
-      printf("Line %d: %s", i, line);
+      // printf("Line %d: %s", i, line);
       i++;
     }
     fclose(fp);
@@ -50,17 +52,14 @@ char** readFile(char fileName[])
   return file;
 }
 
-int main (void)
+unsigned int calcGamma(char** file)
 {
-  char** file = readFile((char *)FILENAME);
-  free(file);
+  int i, j, bit;
+  char gammaRate[MAX_LINE_LENGTH];
   if (file)
   {
-    char gammaRate[MAX_LINE_LENGTH - 1], epsilonRate[MAX_LINE_LENGTH - 1];
-
     /* Determine gamma rate */
-    int i, j, bit;
-    for (i = 0; i < MAX_LINE_LENGTH - 2; i++) {
+    for (i = 0; i < MAX_LINE_LENGTH; i++) {
       bit = 0;
       for (j = 0; j < MAX_LINES; j++) {
         if (file[j][i] - '0' >= 0)
@@ -68,12 +67,43 @@ int main (void)
       }
       gammaRate[i] = round((double)bit / MAX_LINES) + '0';
     }
-    printf("Gamma rate: %s", gammaRate);
+    printf("Gamma rate (binary): %s\n", gammaRate);
   }
   else
   {
     /* Error handling */
     return 0;
   }
+  return strtol(gammaRate, NULL, 2);
+}
+
+char* intToBinary(int integer) {
+  // based on answer by @chux at https://codereview.stackexchange.com/questions/255036/represent-integer-as-binary-in-c  
+  int i;
+  char *binary = malloc(MAX_LINE_LENGTH * sizeof(char));
+
+  for (i = MAX_LINE_LENGTH; --i >= 0; ) {
+    binary[i] = '0' + (integer & 1);
+    integer /= 2;
+  }
+
+  return binary;
+}
+
+unsigned int calcEpsilon(unsigned int gammaRate)
+{
+  int epsilonRate = gammaRate ^ 0b11111;
+  printf("Gamma rate (binary): %s\n", intToBinary(epsilonRate));
+  return epsilonRate;
+}
+
+int main(void)
+{
+  char** file = readFile((char *)FILENAME);
+  unsigned int gammaRate = calcGamma(file);
+  printf("Gamma rate integer: %d\n", gammaRate);
+  unsigned int epsilonRate = calcEpsilon(gammaRate);
+  printf("Epsilon rate integer: %d\n", epsilonRate);
+  free(file);
   return 1;
 }
